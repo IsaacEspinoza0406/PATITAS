91fee1bab16e4f4effc3783aec189720401db787
Vanessa Jiménez Molina <243712@ids.upchiapas.edu.mx>
2025-11-01 19:23:31 -0600

    Se agregó el css y un link aun no funcional en el html de home->donations.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 //Hasta 
 import { FormsComponent } from './pages/forms/forms';
 import { DonationsComponent } from './pages/donations/donations'; 
+import{ Home }  from './pages/home/home';
 
 export const routes: Routes = [
   { path: 'forms', component: FormsComponent },
   { path: 'donations', component: DonationsComponent },
+  {path: 'home', component: Home},
   { path: '', redirectTo: '/donations', pathMatch: 'full' },
 ];
 
