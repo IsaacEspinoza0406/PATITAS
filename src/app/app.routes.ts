6576ac4f12ac281d5b7ea31b199d31bc922fc53f
Emma <reyespreciadoemma@gmail.com>
2025-11-03 08:20:10 -0600

    Se completó el módulo de solicitudes (admin)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
-//Hasta 
+//Hasta
 import { FormsComponent } from './pages/forms/forms';
-import { DonationsComponent } from './pages/donations/donations'; 
+import { DonationsComponent } from './pages/donations/donations';
 import{ Home }  from './pages/home/home';
+import {AdoptionRequestComponent} from './pages/admin/AdoptionRequest/AdoptionRequest';
 
 export const routes: Routes = [
   { path: 'forms', component: FormsComponent },
   { path: 'donations', component: DonationsComponent },
   {path: 'home', component: Home},
   { path: '', redirectTo: '/donations', pathMatch: 'full' },
+  { path: 'AdoptionRequest', component: AdoptionRequestComponent },
 ];
 
