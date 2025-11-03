ef6a21d8275e11507ad5053248951aabaf0e809e
IsaacEspinoza0406 <243751@ids.upchiapas.edu.mx>
2025-11-03 02:05:49 -0600

    Ya quedó el navbar, al fiiiiin.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,25 @@
 import { Routes } from '@angular/router';
-//Hasta 
 import { FormsComponent } from './pages/forms/forms';
 import { DonationsComponent } from './pages/donations/donations'; 
-import{ Home }  from './pages/home/home';
+import { Home } from './pages/home/home'; 
+// import { CatalogComponent } from './pages/catalog/catalog'; 
+// import { AdoptionRequestComponent } from './pages/admin/AdoptionRequest/AdoptionRequest'; 
 
 export const routes: Routes = [
+  { path: 'inicio', component: Home }, 
+  
+  // { path: 'perros', component: CatalogComponent }, 
+  
+  { path: 'donations', component: DonationsComponent }, 
+
   { path: 'forms', component: FormsComponent },
-  { path: 'donations', component: DonationsComponent },
-  {path: 'home', component: Home},
-  { path: '', redirectTo: '/donations', pathMatch: 'full' },
+
+  { path: 'inicia-sesion', component: Home}, 
+  
+  // { path: 'admin', component: AdoptionRequestComponent },
+  
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  
+  { path: '**', redirectTo: '/inicio' } 
 ];
 
