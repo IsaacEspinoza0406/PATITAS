799c2f8ae84df4254e6d782329a2335a857e1d76
Luis Emilio Jaras Sanchez <243697@ids.upchiapas.edu.mx>
2025-11-03 07:22:28 -0600

    las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Routes } from '@angular/router';
 import { FormsComponent } from './pages/forms/forms';
 import { DonationsComponent } from './pages/donations/donations'; 
 import{ Home }  from './pages/home/home';
+import { CatalogComponent } from './pages/admin/catalog/catalog';
 
 export const routes: Routes = [
   { path: 'forms', component: FormsComponent },
   { path: 'donations', component: DonationsComponent },
   {path: 'home', component: Home},
+  { path: 'admin/catalog', component: CatalogComponent },
   { path: '', redirectTo: '/donations', pathMatch: 'full' },
 ];
 
