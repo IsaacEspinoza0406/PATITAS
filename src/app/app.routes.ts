16bf90b9789b4960ee3b423ecb06dc313d11079c
IsaacEspinoza0406 <243751@ids.upchiapas.edu.mx>
2025-11-12 14:28:43 -0600

    Se modificarón las rutas.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,48 +1,41 @@
 import { Routes } from '@angular/router';
 
+// --- Componentes Públicos ---
 import { FormsComponent } from './pages/forms/forms';
 import { DonationsComponent } from './pages/donations/donations';
 import { Home } from './pages/home/home';
 import { DogCatalog } from './pages/dog-catalog/dog-catalog';
 import { CardCatalog } from './pages/card-catalog/card-catalog';
 
+// --- Componentes de Admin ---
 import { AdoptionRequestComponent } from './pages/admin/AdoptionRequest/AdoptionRequest';
 import { CatalogComponent } from './pages/admin/catalog/catalog';
 import { LoginComponent } from './pages/admin/login/login';
 
 // Rutas de la aplicación
 export const routes: Routes = [
-  // Rutas públicas
   { path: 'inicio', component: Home },
-  { path: 'home', component: Home },
   { path: 'donations', component: DonationsComponent },
   { path: 'forms', component: FormsComponent },
-  {path: 'catalog', component: DogCatalog },
-  {path: 'card-catalog', component: CardCatalog },
+  
+  { path: 'perros', component: DogCatalog }, 
+  
+  { path: 'dog-details/:id', component: CardCatalog},
 
- 
-  // Rutas de administración agrupadas bajo /admin
+  { path: 'inicia-sesion', component: LoginComponent },
+
+  
+  // --- Rutas de Admin (agrupadas) ---
   {
     path: 'admin',
     children: [
       { path: 'catalog', component: CatalogComponent },
       { path: 'adoption-request', component: AdoptionRequestComponent },
       { path: 'login', component: LoginComponent },
-      
-      // Por defecto dentro de /admin redirige al catálogo
       { path: '', redirectTo: 'catalog', pathMatch: 'full' }
     ]
   },
 
-  // Redirección por defecto y fallback
-  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
-  { path: '**', redirectTo: '/inicio' },
-  
-
-//Ruta para detalles de perro
-{ path: 'dog-details/:id', component: CardCatalog},
-{ path: '', redirectTo: '/catalog', pathMatch: 'full'}, //Si no hay ruta, redirige a /catalogo
-{ path: '**', redirectTo: '/catalog'},
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' }, 
+  { path: '**', redirectTo: '/inicio' }, 
 ];
-
- 
