6ca09dd56523545002c4b4b58b0b2fc22a66f895
IsaacEspinoza0406 <243751@ids.upchiapas.edu.mx>
2025-11-12 13:04:03 -0600

    Se arregló el catalogo.

--- src/app/pages/card-catalog/card-catalog.ts
+++ src/app/pages/card-catalog/card-catalog.ts
@@ -1,42 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute,Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DogService, Dog } from '../../services/dog-service';
-
+import { CommonModule } from '@angular/common'; 
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-card-catalog',
-  imports: [],
+  standalone: true, 
+  imports: [CommonModule, FormsModule], 
   templateUrl: './card-catalog.html',
   styleUrl: './card-catalog.css'
 })
 export class CardCatalog implements OnInit {
-dog: Dog | undefined;
+  dog: Dog | undefined;
 
-constructor(
-  private route: ActivatedRoute,
-  private router: Router,
-  private dogService: DogService,
-){}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private dogService: DogService,
+  ) { }
 
-ngOnInit(): void {
-  const idFromRoute = this.route.snapshot.paramMap.get('id');
-  if (idFromRoute) {
-    const dogId=+idFromRoute;
-    this.dog=this.dogService.getDogById(dogId);
-    if (!this.dog) {
-      // Si no se encuentra el perro, redirigir al catálogo
-      this.router.navigate(['/catalog']);
-    }else{
+  ngOnInit(): void {
+    const idFromRoute = this.route.snapshot.paramMap.get('id');
+
+    if (!idFromRoute) {
       console.error('ID de perro no proporcionado en la ruta');
+      this.router.navigate(['/catalog']);
+      return; 
+    }
+
+    const dogId = +idFromRoute;
+    this.dog = this.dogService.getDogById(dogId);
+
+    if (!this.dog) {
+      console.error(`Perro con ID ${dogId} no encontrado.`);
       this.router.navigate(['/catalog']);
     }
   }
-}
-//Lógica para el botón "Volver"
-onBack(): void {
-  this.router.navigate(['/catalog']);
-}
-ondopt(): void {
-  console.log('Iniciar proceso de adopción para el perro con ID:', this.dog?.id);
-}
+
+  onBack(): void {
+    this.router.navigate(['/catalog']);
+  }
+
+  onAdopt(): void {
+    console.log('Iniciar proceso de adopción para el perro con ID:', this.dog?.id);
+  }
 }
