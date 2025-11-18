e2f5f307a43baca488635dfafb2ae9ef6a759b15
IsaacEspinoza0406 <243751@ids.upchiapas.edu.mx>
2025-11-18 02:14:11 -0600

    Se crearón los archivos para hacer la parte del login..

---
+++ src/app/pages/auth-selection/auth-selection.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-selection',
+  imports: [],
+  templateUrl: './auth-selection.html',
+  styleUrl: './auth-selection.css'
+})
+export class AuthSelection {
+
+}
