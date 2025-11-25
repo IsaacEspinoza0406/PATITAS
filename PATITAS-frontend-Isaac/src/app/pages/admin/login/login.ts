46161eb9e4e578adcc8907f1f6d5a1d219523f00
EmilioJaras3 <243697@ids.upchiapas.edu.mx>
2025-11-25 13:08:27 -0600

    ya quedo

--- PATITAS-frontend-Isaac/src/app/pages/admin/login/login.ts
+++ PATITAS-frontend-Isaac/src/app/pages/admin/login/login.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule,],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
   styleUrls: ['./login.css']
 })
 export class LoginComponent {
 
-  // Con esta variable controlamos si estamos en modo Login o Registro.
-  isRegisterMode = false;
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
   // Datos del formulario.
   loginData = {
@@ -20,18 +22,26 @@ export class LoginComponent {
     password: ''
   };
 
-  registerData = {
-    userName: '',
-    email: '',
-    password: '',
-    confirmPassword: ''
-  };
-
-
-
   // Iniciar sesión.
   onLogin() {
-    console.log('Intentando iniciar sesión con:', this.loginData);
-    alert('¡Iniciando sesión!');
+    console.log('Intentando iniciar sesión como Admin:', this.loginData);
+
+    this.authService.login(this.loginData).subscribe({
+      next: (response: any) => {
+        console.log('Login exitoso:', response);
+
+        if (response.user && response.user.roleId === 1) {
+          alert('¡Bienvenido Administrador!');
+          this.router.navigate(['/admin/adoption-request']);
+        } else {
+          alert('Acceso denegado. No tienes permisos de administrador.');
+          this.authService.logout();
+        }
+      },
+      error: (error: any) => {
+        console.error('Error login:', error);
+        alert('Credenciales incorrectas o error en el servidor.');
+      }
+    });
   }
 }
