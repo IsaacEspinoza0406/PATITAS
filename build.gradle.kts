d50a67ba2b612a1dd10cd9093661095cdaf34044
EmilioJaras3 <243697@ids.upchiapas.edu.mx>
2025-12-01 22:03:33 -0600

    Arreglar conflicto de versiones de Gradle Shadow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "2.0.20"
     id("io.ktor.plugin") version "2.3.12"
-    id("com.gradleup.shadow") version "8.3.8"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "com.patitas_web"
