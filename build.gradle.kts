dac1bf3ec1effbbe3723d14bbe9164e7964fdacb
EmilioJaras3 <243697@ids.upchiapas.edu.mx>
2025-12-02 00:58:41 -0600

    Fix: Add kotlin serialization plugin to resolve 500/400 errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.20"
+    kotlin("plugin.serialization") version "2.0.20"
     id("io.ktor.plugin") version "2.3.12"
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
